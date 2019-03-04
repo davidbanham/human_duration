b6215f4bb49ce5db11a2fa67c1e8c2f60ea6b8eb
David Banham <david@banham.id.au>
2019-03-04 11:35:22 +1100

    Simplify test code

--- main_test.go
+++ main_test.go
@@ -18,62 +18,62 @@ func TestString(t *testing.T) {
 	year := day * 365
 
 	data := []fixture{
-		fixture{
+		{
 			duration:  time.Hour*25 + time.Minute*4 + time.Second*8,
 			precision: "second",
 			result:    "1 day 4 minutes 8 seconds",
 		},
-		fixture{
+		{
 			duration:  time.Hour*25 + time.Minute*4 + time.Second*8,
 			precision: "minute",
 			result:    "1 day 4 minutes",
 		},
-		fixture{
+		{
 			duration:  time.Hour*25 + time.Minute*4 + time.Second*8,
 			precision: "day",
 			result:    "1 day",
 		},
-		fixture{
+		{
 			duration:  year*4 + day*2,
 			precision: "second",
 			result:    "4 years 2 days",
 		},
-		fixture{
+		{
 			duration:  time.Minute * 60,
 			precision: "hour",
 			result:    "1 hour",
 		},
-		fixture{
+		{
 			duration:  time.Minute * 60,
 			precision: "minute",
 			result:    "1 hour",
 		},
-		fixture{
+		{
 			duration:  time.Minute * 61,
 			precision: "minute",
 			result:    "1 hour 1 minute",
 		},
-		fixture{
+		{
 			duration:  (time.Minute * 61) + (time.Second * 10),
 			precision: "minute",
 			result:    "1 hour 1 minute",
 		},
-		fixture{
+		{
 			duration:  (time.Minute * 61) + (time.Second * 10),
 			precision: "second",
 			result:    "1 hour 1 minute 10 seconds",
 		},
-		fixture{
+		{
 			duration:  time.Hour * 23,
 			precision: "day",
 			result:    "1 day",
 		},
-		fixture{
+		{
 			duration:  time.Hour * 49,
 			precision: "day",
 			result:    "2 days",
 		},
-		fixture{
+		{
 			duration:  time.Hour * 49,
 			precision: "hour",
 			result:    "2 days 1 hour",
